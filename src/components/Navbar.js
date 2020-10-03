8f4bfdda2ee3225f184ad588e3216d3a9a587859
Bruna <brunagenz92@gmail.com>
2020-10-03 11:25:34 -0300

    Feat: add bootstrap and configurations to use it; create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const NavbarComponent = () => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">Dr. Camila Genz</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Antendimentos</Nav.Link>
+        <Nav.Link href="#link">Curriculo</Nav.Link>
+        <Nav.Link href="#link">Contato</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
+
+export default NavbarComponent;
